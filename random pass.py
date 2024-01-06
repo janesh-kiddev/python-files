aa91e2cda5988494b0f6d25ca3bb3fe141a5040a
JANESHNANDA K S <137386817+janesh-kiddev@users.noreply.github.com>
2024-01-06 18:33:20 +0530

    Update random pass.py

--- random pass.py
+++ random pass.py
@@ -1,14 +1,23 @@
 import random
 
 def password_generator(number_characters):
-    si=""
-    sj=""
-    for i in range(1,number_characters+1):
-        si+=str(i)
-    for j in range(1,number_characters+1):
-        sj+=str(j)
+        str_forward=""
+        str_backward=""
+        character=""
+        for forward in range(1,number_characters+1):
+            str_forward+=str(forward)
+        for backward in range(1,number_characters+1):
+            str_backward+=str(backward)
 
-    print(random.randrange(int(si),int(sj[::-1])))
+        return random.randrange(int(str_forward),int(str_backward[::-1]))
+while True:
+    try:
+        number_characters=int(input("Length of password: "))
+        if number_characters<4:
+            raise Exception
+    except (Exception,ValueError):
+        print('Not In Range please  A Try Number')
+    else:
+        print(password_generator(number_characters))
+        break
 
-number_characters=int(input("Length of password: "))
-password_generator(number_characters)
