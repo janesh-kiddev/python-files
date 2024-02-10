b4f0d656cc1b9c33ed41e7311f6cf5b319e5e341
JANESHNANDA K S <137386817+janesh-kiddev@users.noreply.github.com>
2024-02-10 09:12:13 +0530

    Update random pass.py

--- random pass.py
+++ random pass.py
@@ -1,22 +1,33 @@
 import random
 
-def password_generator(number_characters):
-        str_forward=""
-        str_backward=""
-        for forward in range(1,number_characters+1):
-            str_forward+=str(forward)
-        for backward in range(1,number_characters+1):
-            str_backward+=str(backward)
 
-        return random.randrange(int(str_forward),int(str_backward[::-1]))
+string = "PASSWORD WITH NUMBERS"
+new_string = string.center(25, '$')
+print(new_string)
+
+
+def password_generator(number_characters):
+        str_forward = ""
+        str_backward = ""
+        for forward in range(1,number_characters+1):
+            str_forward += str(forward)
+        for backward in range(1,number_characters+1):
+            str_backward += str(backward)
+
+        return random.randrange (int(str_forward),int(str_backward[::-1]))
+
 while True:
     try:
-        number_characters=int(input("Length of password: "))
-        if number_characters<4:
+        number_characters = int(input("Set the length of password greater than 4 and less than 16: ").title())
+        if number_characters < 4:
             raise Exception
-    except (Exception,ValueError):
-        print('Not In Range please  A Try Number')
+        if number_characters > 16:
+            raise Exception
+    except(Exception,ValueError):
+        print('Please a Try Number')
     else:
-        print(password_generator(number_characters))
+        val = str(password_generator(number_characters))
+        print(f"THE PASSWORD IS {val}")
         break
+
 
