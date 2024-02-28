8d15544f62786bcbce4cb0d0b13b11b2fc3c02be
JANESHNANDA K S <137386817+janesh-kiddev@users.noreply.github.com>
2024-02-28 11:50:40 +0530

    Update qr_generator.py

--- samplfile/qr_generator.py
+++ samplfile/qr_generator.py
@@ -11,4 +11,6 @@ def generate(text):
     image = qr.make_image(fill_color = 'black',back_color = 'white')
     image.save("qr_img.png")
 
-generate("https://www.apple.com/in/store?afid=p238%7Cs8Vs8GkTq-dc_mtid_187079nc38483_pcrid_689251521289_pgrid_112258962467_pntwk_g_pchan__pexid__&cid=aos-IN-kwgo-brand--slid---product")
+
+url = input("Enter url :")
+generate(url)
