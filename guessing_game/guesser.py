bd089ac1408fb62d159aa2f48dd067d0445291bd
Janeshnanda k s <137386817+janesh-kiddev@users.noreply.github.com>
2024-07-19 20:36:37 +0530

    Update guesser.py

--- guessing_game/guesser.py
+++ guessing_game/guesser.py
@@ -1,10 +1,14 @@
 import random
 print("#####Guesses#####".center(100))
 print("This is a game where the task is find the number within 10 tries".title())
+
 real_number = random.randint(0,100)
 tries = 10
 c = 0
 while True:
+    if c == tries:
+        print(f"TRY AGAIN NEXT TIME the number was {real_number}")
+        break
     c+=1
     find = int(input(f'find the number:'))
     if find == real_number:
@@ -14,6 +18,3 @@ while True:
         print('The number is larger')
     elif real_number < find:
         print('The number is lower')
-    elif c == tries:
-        print(f"TRY AGAIN NEXT TIME the number was {real_number}")
-        break
