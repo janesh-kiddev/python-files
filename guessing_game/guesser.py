4e05edc4f535a8e81ff8bd07fb76a86a688ed138
Janeshnanda k s <137386817+janesh-kiddev@users.noreply.github.com>
2024-07-19 20:44:12 +0530

    Update guesser.py

--- guessing_game/guesser.py
+++ guessing_game/guesser.py
@@ -2,19 +2,26 @@ import random
 print("#####Guesses#####".center(100))
 print("This is a game where the task is find the number within 10 tries".title())
 
-real_number = random.randint(0,100)
+thrsh = int(input("set threshold for finding the value:").title())
+
+real_number = random.randint(0,thrsh)
 tries = 10
+
 c = 0
+
 while True:
     if c == tries:
         print(f"TRY AGAIN NEXT TIME the number was {real_number}")
         break
+    
     c+=1
     find = int(input(f'find the number:'))
     if find == real_number:
         print(f"found at {c} tries the number is {real_number}")
         break
+    
     elif real_number > find:
         print('The number is larger')
+    
     elif real_number < find:
         print('The number is lower')
